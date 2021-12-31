202fbee8414e9bf69ee36c0d7e1bbb1a63f2aba6
Brian Chau <brian.chau@outlook.com>
2021-12-30 17:54:40 -0800

    Reverted main.cpp file.

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,14 +7,6 @@
 
 #include "http_gets.h"
 
-std::map<std::string, std::string> parse_html(std::string html, std::regex regex_pattern, std::map<std::string, std::string> chapter_map) {
-    int chapter(0);
-    while(std::regex_search(html, m, regex_pattern)) {
-        chapter_map["0" + std::to_string(chapter++)] = m[1].str();
-        html = m.suffix();
-    }
-}
-
 int main( int argc, char** argv ) {
     std::string base_url("https://pages.cs.wisc.edu/~remzi/OSTEP/");
     std::unique_ptr<http_gets> curl = std::unique_ptr<http_gets>(new http_gets(base_url,80));
@@ -26,9 +18,19 @@ int main( int argc, char** argv ) {
     std::smatch m;
 
     std::string html = curl->download_html();
+    std::string html2(html);
+
+    int chapter(0);
     std::map<std::string, std::string> chapter_map;
-    parse_html(html, chapterless_regex, chapter_map);
-    parse_html(html, chapter_regex,     chapter_map);
+    while(std::regex_search(html, m, chapterless_regex)) {
+        chapter_map["0" + std::to_string(chapter++)] = m[1].str();
+        html = m.suffix();
+    }
+
+    while(std::regex_search(html2, m, chapter_regex)) {
+        chapter_map[m[1].str()] = m[3].str();
+        html2 = m.suffix();
+    }
 
     for (auto const& [chapter, filename] : chapter_map) {
         std::cout << filename << std::endl;
